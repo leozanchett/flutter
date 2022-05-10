9a45cf1cd240468b9eca22ff2e33a6e039c18a22
Leo <45577227+leozanchett@users.noreply.github.com>
2022-05-10 15:28:10 -0300

    Statefull

--- 2022/jacob_yt/helloworld/lib/home_page.dart
+++ 2022/jacob_yt/helloworld/lib/home_page.dart
@@ -3,14 +3,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:helloworld/myapp.dart';
 
-import 'home_controller.dart';
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
 
-class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final HomeController controller = HomeController.of(context);
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.amber,
+//         title: const Text('Home'),
+//         systemOverlayStyle: SystemUiOverlayStyle.dark,
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           if (kDebugMode) {
+//             controller.increment();
+//           }
+//         },
+//         child: const Icon(Icons.add),
+//         backgroundColor: Colors.amber,
+//       ),
+//       body: MyApp(value: controller.value),
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    if (kDebugMode) {
+      print('initState');
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (kDebugMode) {
+      print('dispose');
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final HomeController controller = HomeController.of(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber,
@@ -20,13 +66,13 @@ class HomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (kDebugMode) {
-            controller.increment();
+            setState(() => counter++);
           }
         },
         child: const Icon(Icons.add),
         backgroundColor: Colors.amber,
       ),
-      body: MyApp(value: controller.value),
+      body: MyApp(value: counter),
     );
   }
 }
