7fb5657dbd64b50941717ab33611c816ab8d84e0
Leo <45577227+leozanchett@users.noreply.github.com>
2022-05-12 11:00:44 -0300

    Contador de requests

--- 2022/jacob_yt/hutCidade/hutcidade/lib/services/database.dart
+++ 2022/jacob_yt/hutCidade/hutcidade/lib/services/database.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  static int requestCount = 0;
+
   // collection reference
   static final CollectionReference onibus = FirebaseFirestore.instance.collection('/onibus/horarios/2022');
   static final CollectionReference trash = FirebaseFirestore.instance.collection('/trash/horarios/2022');
 
   static Future<DocumentSnapshot<Object?>?> getOnibusHorarios({required String cidade, required String bairro, required bool isBus}) async {
+    requestCount++;
     try {
       DocumentSnapshot snapshot;
       if (isBus) {
