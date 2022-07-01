90fef6425d1c5d28a63928a6af69bfac25c375f3
Leo <45577227+leozanchett@users.noreply.github.com>
2022-07-01 10:49:38 -0300

    feat
    Adicionado ADBanner na home.

--- 2022/jacob_yt/hutCidade/hutcidade/lib/layout/body-home.dart
+++ 2022/jacob_yt/hutCidade/hutcidade/lib/layout/body-home.dart
@@ -16,6 +16,7 @@ class BodyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     final ConfigController _cfgController = Get.find<ConfigController>();
     final ADMobService _adMobService = Get.find<ADMobService>();
+
     return GetX<HorarioController>(
       init: HorarioController(),
       initState: (_) async {
@@ -32,102 +33,100 @@ class BodyHome extends StatelessWidget {
       builder: (_horarioController) {
         return Container(
           color: Colors.grey.withOpacity(0.95),
-          child: Column(
-            children: [
-              SizedBox(
-                  child: DatePicker(
-                DateTime.now(),
-                width: 60,
-                height: 80,
-                initialSelectedDate: DateTime.now(),
-                selectionColor: Colors.black,
-                selectedTextColor: Colors.white,
-                onDateChange: (date) {
-                  _cfgController.setDataRequest(date);
-                  _horarioController.fetch_lista_horarios(_cfgController.config.value);
-                  _adMobService.showInterstitialAd();
-                },
-                locale: 'pt_BR',
-              )),
-              _horarioController.loading.value
-                  ? Expanded(
-                      child: SizedBox(
-                        child: Center(
-                          child: CircularProgressIndicator(
-                            backgroundColor: Colors.transparent,
-                            color: Theme.of(context).primaryColor,
-                          ),
+          child: Column(children: [
+            SizedBox(
+                child: DatePicker(
+              DateTime.now(),
+              width: 60,
+              height: 80,
+              initialSelectedDate: DateTime.now(),
+              selectionColor: Colors.black,
+              selectedTextColor: Colors.white,
+              onDateChange: (date) {
+                _cfgController.setDataRequest(date);
+                _horarioController.fetch_lista_horarios(_cfgController.config.value);
+                _adMobService.showInterstitialAd();
+              },
+              locale: 'pt_BR',
+            )),
+            _horarioController.loading.value
+                ? Expanded(
+                    child: SizedBox(
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.transparent,
+                          color: Theme.of(context).primaryColor,
                         ),
                       ),
-                    )
-                  : SizedBox(
-                      height: Get.height / 1.5,
-                      child: ListWheelScrollView(
-                        itemExtent: 60,
-                        diameterRatio: 1.5,
-                        perspective: RenderListWheelViewport.defaultPerspective,
-                        children: <Widget>[
-                          // Obx(
-                          //   () => (!_adMobService.bannerAdIsReady.value)
-                          //       ? SizedBox(
-                          //           width: Get.width,
-                          //           child: Card(
-                          //             shape: RoundedRectangleBorder(
-                          //               borderRadius: BorderRadius.circular(20),
-                          //             ),
-                          //             child: SizedBox(
-                          //               width: _adMobService.bannerAd.size.width.toDouble(),
-                          //               height: _adMobService.bannerAd.size.height.toDouble(),
-                          //               child: AdWidget(
-                          //                 ad: _adMobService.bannerAd,
-                          //               ),
-                          //             ),
-                          //           ),
-                          //         )
-                          //       : Container(
-                          //           width: 0,
-                          //           color: Colors.transparent,
-                          //         ),
-                          // ),
-                          for (Horarios horario in _horarioController.listaHorarios)
-                            SizedBox(
-                              width: Get.width,
-                              child: Card(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: ListTile(
-                                  title: Text(horario.hora),
-                                  leading: const Icon(
-                                    Icons.access_time_sharp,
-                                    color: Colors.black87,
-                                  ),
-                                  trailing: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text(horario.rota, style: const TextStyle(fontSize: 16)),
-                                      const SizedBox(
-                                        width: 10,
+                    ),
+                  )
+                : SizedBox(
+                    height: Get.height / 1.5,
+                    child: ListWheelScrollView(
+                      itemExtent: 60,
+                      diameterRatio: 1.5,
+                      perspective: RenderListWheelViewport.defaultPerspective,
+                      children: <Widget>[
+                        Obx(
+                          () => (_adMobService.bannerAdIsReady.value)
+                              ? SizedBox(
+                                  width: Get.width,
+                                  child: Card(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: SizedBox(
+                                      width: _adMobService.bannerAd.size.width.toDouble(),
+                                      height: _adMobService.bannerAd.size.height.toDouble(),
+                                      child: AdWidget(
+                                        ad: _adMobService.bannerAd,
                                       ),
-                                      _cfgController.config.value.selectedRequest == SelectedRequest.bus
-                                          ? const Icon(
-                                              Icons.directions_outlined,
-                                              color: Colors.black87,
-                                            )
-                                          : const Icon(
-                                              Icons.delete_outlined,
-                                              color: Colors.black87,
-                                            ),
-                                    ],
+                                    ),
                                   ),
+                                )
+                              : Container(
+                                  width: 0,
+                                  color: Colors.transparent,
+                                ),
+                        ),
+                        for (Horarios horario in _horarioController.listaHorarios)
+                          SizedBox(
+                            width: Get.width,
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: ListTile(
+                                title: Text(horario.hora),
+                                leading: const Icon(
+                                  Icons.access_time_sharp,
+                                  color: Colors.black87,
+                                ),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(horario.rota, style: const TextStyle(fontSize: 16)),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    _cfgController.config.value.selectedRequest == SelectedRequest.bus
+                                        ? const Icon(
+                                            Icons.directions_outlined,
+                                            color: Colors.black87,
+                                          )
+                                        : const Icon(
+                                            Icons.delete_outlined,
+                                            color: Colors.black87,
+                                          ),
+                                  ],
                                 ),
                               ),
-                            )
-                        ],
-                      ),
+                            ),
+                          )
+                      ],
                     ),
-            ],
-          ),
+                  ),
+          ]),
         );
       },
     );
